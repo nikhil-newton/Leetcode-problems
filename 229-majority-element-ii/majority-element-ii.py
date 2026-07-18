4b170920380f23d220edc642c906ff2108e6cad7
nikhil-newton <23MH1A4237@acoe.edu.in>
2026-07-18 13:24:00 +0530

    Time: 16 ms (24.62%) | Memory: 14.9 MB (47.51%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -2,4 +2,4 @@ class Solution(object):
     def majorityElement(self, nums):
         from collections import Counter 
         counts = Counter(nums)
-        return list(num for num in counts if counts[num] > len(nums)/3)
+        return [num for num in counts if counts[num] > len(nums)/3]
